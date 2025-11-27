6848277153aef0ce62166d4d7bf4a29cd9570ea3
Rachael Momber <109778611+RLMP44@users.noreply.github.com>
2025-11-27 04:22:59 +0000

    add: logic for registering vs logging in

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "./Login";
 
-var isLoggedIn = false;
+var isRegistered = false;
 
 function App() {
   return (
-    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    <div className="container">
+      <Login reg={isRegistered} />
+    </div>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Input from "./Input";
 
-function Login() {
+function Login(props) {
   return (
     <form className="form">
       <Input type="text" placeholder="Username" />
       <Input type="password" placeholder="Password" />
-      <button type="submit">Login</button>
+      {!props.reg && <Input type="password" placeholder="Confirm password" />}
+      <button type="submit">{props.reg ? "Login" : "Register"}</button>
     </form>
   );
 }
