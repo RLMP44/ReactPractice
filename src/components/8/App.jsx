ad54c592adf7731e68dfa3b9138f606bd04f75c0
Rachael Momber <109778611+RLMP44@users.noreply.github.com>
2025-12-06 18:35:08 +0000

    refactor: reuse same function for value changes

--- src/components/8/App.jsx
+++ src/components/8/App.jsx
@@ -1,33 +1,46 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
 
-  function updateFName(event) {
-    setFName(event.target.value);
-  }
-  function updateLName(event) {
-    setLName(event.target.value);
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+    setFullName((previousValue) => {
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: previousValue.lName,
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: previousValue.fName,
+          lName: newValue,
+        };
+      }
+    });
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {fName} {lName}
+        Hello {fullName.fName} {fullName.lName}
       </h1>
       <form>
         <input
-          onChange={updateFName}
+          onChange={handleChange}
           name="fName"
           placeholder="First Name"
-          value={fName}
+          value={fullName.fName}
         />
         <input
-          onChange={updateLName}
+          onChange={handleChange}
           name="lName"
           placeholder="Last Name"
-          value={lName}
+          value={fullName.lName}
         />
         <button>Submit</button>
       </form>
