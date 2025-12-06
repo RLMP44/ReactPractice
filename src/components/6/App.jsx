6001668df2f50c0d0d3d4faf409dbc75c2dcf100
Rachael Momber <109778611+RLMP44@users.noreply.github.com>
2025-12-06 15:03:28 +0000

    feat: make form functional using hooks

--- src/components/6/App.jsx
+++ src/components/6/App.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 
 function App() {
-  const [headingText, setHeadingText] = useState("Hello");
+  const [buttonText, setButtonText] = useState("Submit");
   const [isMouseOver, setMouseOver] = useState(false);
-  function handleClick() {
-    setHeadingText("Submitted");
+  // name is to be used as a controlled component
+  // it must also be set as the value of the input to ensure consistency
+  // using react as a "single source of truth"
+  // https://legacy.reactjs.org/docs/forms.html#controlled-components
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function handleClick(event) {
+    setButtonText("Submitted");
+    setHeadingText(name);
+    setTimeout(() => {
+      setButtonText("Submit");
+    }, 1500);
+    event.preventDefault();
   }
   function handleMouseOver() {
     setMouseOver(true);
@@ -12,23 +24,56 @@ function App() {
   function handleMouseOut() {
     setMouseOver(false);
   }
+  function handleChange(event) {
+    setName(event.currentTarget.value);
+  }
 
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        style={{
-          backgroundColor: isMouseOver ? "black" : "white",
-        }}
-        onClick={handleClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        Submit
-      </button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button
+          type="submit"
+          style={{
+            backgroundColor: isMouseOver ? "black" : "white",
+          }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          {buttonText}
+        </button>
+      </form>
     </div>
   );
+}
+
+// alternate form submission
+{
+  /* <form>
+  <input
+    onChange={handleChange}
+    type="text"
+    placeholder="What's your name?"
+    value={name}
+  />
+  <button
+    type="submit"
+    style={{
+      backgroundColor: isMouseOver ? "black" : "white",
+    }}
+    onClick={handleClick}
+    onMouseOver={handleMouseOver}
+    onMouseOut={handleMouseOut}
+  >
+    {buttonText}
+  </button>
+</form>; */
 }
 
 export default App;
